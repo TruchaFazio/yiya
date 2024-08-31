9415bf79a960987d1e970b132cd1d9e2f6073ecd
Santiago Fazio <santiago.fazio@yahoo.com>
2024-08-31 14:25:20 -0300

    fisicas movimiento

--- scripts/player.gd
+++ scripts/player.gd
@@ -2,15 +2,21 @@ extends CharacterBody2D
 
 @export var move_speed: float
 @export var jump_speed: float
+@export var gravity: float
 var is_facing_rigth = true
 
 func _physics_process(delta):
-	if Input.is_action_just_pressed("jump"):
-		velocity.y = -jump_speed
+	if not is_on_floor():
+		velocity.y += gravity * delta
+		
+	jump()
 	move_x()
 	flip()
 	move_and_slide()
-	
+
+func jump():
+	if Input.is_action_just_pressed("jump") and is_on_floor():
+		velocity.y = -jump_speed
 
 func move_x():
 	var input_axis = Input.get_axis("move_left", "move_rigth")
