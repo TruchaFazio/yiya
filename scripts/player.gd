41844d99703e09ab686fcc06c8f0c816a3272d80
Santiago Fazio <santiago.fazio@yahoo.com>
2024-08-31 19:01:14 -0300

    animaciones

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,5 +1,6 @@
 extends CharacterBody2D
 
+@onready var animated_sprite = $PinkMonsterReposo
 @export var move_speed: float
 @export var jump_speed: float
 @export var gravity: float
@@ -12,7 +13,21 @@ func _physics_process(delta):
 	jump()
 	move_x()
 	flip()
+	update_animations()
 	move_and_slide()
+	
+func update_animations():
+	if not is_on_floor():
+		if velocity.y < 0:
+			animated_sprite.play("jump")
+		else:
+			animated_sprite.play("reposo")
+		return
+				
+	if velocity.x:
+		animated_sprite.play("run")
+	else:
+		animated_sprite.play("reposo")
 
 func jump():
 	if Input.is_action_just_pressed("jump") and is_on_floor():
