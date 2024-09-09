aa635b52bc7ead7f8a02de34734f427f94f9c6ee
Santiago Fazio <santiago.fazio@yahoo.com>
2024-09-08 22:33:21 -0300

    edicion de mapa

--- scripts/coin.gd
+++ scripts/coin.gd
@@ -1,12 +1,9 @@
 extends Area2D
 
 
-# Called when the node enters the scene tree for the first time.
-func _ready() -> void:
-	print("+1 coin on map")
+func _on_body_entered(body: Node2D) -> void:
+	print("+1 coin")
+	queue_free()
 	
-
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
+	
+	
